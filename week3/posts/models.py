27518fd6a3e5b60bcbc3de5bf204b7b39060fd1f
only4wxx <orchidbyw1@gmail.com>
2024-04-02 23:46:47 +0900

    [FIX] 수정한 ERD에 맞게 필드 수정

--- week3/posts/models.py
+++ week3/posts/models.py
@@ -20,14 +20,14 @@ class Post(BaseModel):
     )
 
     id = models.AutoField(primary_key=True)
-    title = models.CharField(verbose_name="제목", max_length=20)
+    title = models.CharField(verbose_name="제목", max_length=50)
     content = models.TextField(verbose_name="내용")
-    writer = models.CharField(verbose_name="작성자", max_length=10)
-    category = models.CharField(choices=CHOICES, max_length=20)
+    writer = models.IntegerField(verbose_name="작성자")
+    category = models.CharField(choices=CHOICES, max_length=30)
 
 class Comment(BaseModel):
 
     id = models.AutoField(primary_key=True)
     post_id = models.IntegerField(verbose_name="게시글 ID")
     content = models.TextField(verbose_name="내용")
-    writer = models.CharField(verbose_name="작성자", max_length=10)
+    writer = models.IntegerField(verbose_name="작성자")
