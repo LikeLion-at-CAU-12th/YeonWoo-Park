6ce7848343be17942ab52267035ef9c07241ee14
only4wxx <orchidbyw1@gmail.com>
2024-05-03 23:54:33 +0900

    [FEAT] Comment Model Serializer 추가

--- session/posts/serializers.py
+++ session/posts/serializers.py
@@ -1,12 +1,17 @@
 ###Model Serializer case
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
-
-  class Meta:
+    
+    class Meta:
 		# 어떤 모델을 시리얼라이즈할 건지
-    model = Post
+        model = Post
 		# 모델에서 어떤 필드를 가져올지
 		# 전부 가져오고 싶을 때
-    fields = "__all__"
+        fields = "__all__"
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
