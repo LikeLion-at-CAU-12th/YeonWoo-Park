3cf3456e8e0b15741123ccdbf418e1307e086456
only4wxx <orchidbyw1@gmail.com>
2024-05-05 17:47:29 +0900

    [FEAT] post_detail 함수로 구현했던 API GenericAPIview로 구현

--- session/posts/views.py
+++ session/posts/views.py
@@ -219,3 +219,16 @@ class PostList(mixins.CreateModelMixin, mixins.ListModelMixin, generics.GenericA
     
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+class PostDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
