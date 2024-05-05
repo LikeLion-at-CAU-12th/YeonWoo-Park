76d5248b6fb83367c012f26a73f3b2a8fd96159c
only4wxx <orchidbyw1@gmail.com>
2024-05-05 17:42:03 +0900

    [FEAT] PostList 클래스를 통한 API를 url 매핑

--- session/posts/urls.py
+++ session/posts/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     # path('', hello_world, name = 'hello_world'),
     path('introduction', work_standard, name="introduction"),
     path('page', work_challenge, name="page"),
-    path('', post_list, name="post_list"), # Post 전체 조회
+    # path('', post_list, name="post_list"), # Post 전체 조회
+    path('', PostList.as_view()),
     path('week/', a_week_post_list, name="최근 일주일 동안 작성된 게시글 목록 조회"),
     path('<int:id>/', post_detail, name = "게시글 조회"),
     # path('<int:id>/comment/', get_comments_of_post, name = "게시글의 모든 코멘트 조회")
