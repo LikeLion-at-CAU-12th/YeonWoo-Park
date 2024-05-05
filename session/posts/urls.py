6dbb85bd932a888b9ea36b793b2e38525d656d48
only4wxx <orchidbyw1@gmail.com>
2024-05-05 17:49:16 +0900

    [FEAT] PostDetail 클래스를 통한 API를 url 매핑

--- session/posts/urls.py
+++ session/posts/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # path('', post_list, name="post_list"), # Post 전체 조회
     path('', PostList.as_view()),
     path('week/', a_week_post_list, name="최근 일주일 동안 작성된 게시글 목록 조회"),
-    path('<int:id>/', post_detail, name = "게시글 조회"),
+    # path('<int:id>/', post_detail, name = "게시글 조회"),
+    path('<int:id>/', PostDetail.as_view()),
     # path('<int:id>/comment/', get_comments_of_post, name = "게시글의 모든 코멘트 조회")
     path('<int:id>/comment/', GetCommentsOfPost.as_view())
 ]
