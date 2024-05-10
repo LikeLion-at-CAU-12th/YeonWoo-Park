d5d096f784a59277074a87fed34146d68c286ef3
only4wxx <orchidbyw1@gmail.com>
2024-05-10 22:05:09 +0900

    [FEAT] 회원가입 구현

---
+++ session/accounts/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework_simplejwt.serializers import RefreshToken
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer): # ModelSerializer를 상속받음
+    password = serializers.CharField(required=True) # serializer를 통해 패스워드가 맞는지 확인
+    username = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['password', 'username', 'email'] # 위에 적은 필드는 아래에도 꼭 적어줘야 함
+    
+    def save(self, request):
+        user = User.objects.create(
+            username=self.validated_data['username'], # 검증된 데이터 중 username이라는 키 값을 가진 값을 가져옴
+            email=self.validated_data['email'],
+        )
+        
+        # password 암호화
+        user.set_password(self.validated_data['password']) # set_password로 암호화
+        user.save()
+
+        return user
+    
+    def validate(self, data): # 중복 email로 가입하는 것을 방지하기 위해 검증하는 메서드
+        # email = data["email"] # 이는 email에 해당하는 값이 없으면 키에러가 뜸. 따라서 아래처럼 써주는 것
+        email = data.get('email', None)
+        
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('email already exists')
+        
+        return data
--- session/accounts/views.py
+++ session/accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework_simplejwt.serializers import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from accounts.serializers import *
+
+class RegisterView(APIView):
+
+    def post(self, request): # 회원가입을 수행
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True): # is_valid()로 유효성 검사
+            user = serializer.save(request)
+            token = RefreshToken.for_user(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "register success",
+                    "token": {
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
+            return res
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
