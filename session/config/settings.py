c7b095a9008312927e4808867f85b8eacdde9f7d
only4wxx <orchidbyw1@gmail.com>
2024-06-07 19:10:24 +0900

    [Feat] 로컬 환경 MySQL 연결

--- session/config/settings.py
+++ session/config/settings.py
@@ -122,13 +122,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'likelion12th',
+		'USER': 'root', # root로 접속하여 DB를 만들었다면 'root'
+		'PASSWORD': '0921',
+		'HOST': 'localhost',
+		'PORT': '3306', # default mysql portnumber
+	}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
